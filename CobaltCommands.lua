c53c3c34dbbcd9148b4cd4e064e280079fd337e5
prestonelam2003 <prestonelam03@gmail.com>
2020-06-08 08:58:34 -0400

    Added Template to CobaltCommands.lua

---
+++ CobaltCommands.lua
@@ -0,0 +1,42 @@
+--Created by Preston Elam (CobaltTetra) 2020
+--THIS SCRIPT IS PROTECTED UNDER AN GPLv3 LICENSE
+--ADDITIONALLY, YOU MAY EDIT THIS SCRIPT, BUT BY USING IT YOU AGREE TO NOT REMOVE THE CREDIT ON THE FIRST LINE IF IT IS RESDITRIBUTED, YOUR OWN CREDIT MAY BE ADDED ON LINE2.
+
+local M = {}
+
+
+----------------------------------------------------------EVENTS-----------------------------------------------------------
+
+--runs when the script is called.
+local function onInit()
+    
+end
+
+
+
+----------------------------------------------------------MUTATORS---------------------------------------------------------
+
+
+
+---------------------------------------------------------ACCESSORS---------------------------------------------------------
+
+
+
+---------------------------------------------------------FUNCTIONS---------------------------------------------------------
+
+
+
+------------------------------------------------------PUBLICINTERFACE------------------------------------------------------
+
+M.onInit = onInit
+
+----UPDATERS-----
+
+----MUTATORS-----
+
+----ACCESSORS----
+
+----FUNCTIONS----
+
+
+return M
