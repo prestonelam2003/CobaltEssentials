fb1a62016e62b38ed7419c52cce721c388788321
prestonelam2003 <prestonelam03@gmail.com>
2020-06-24 15:39:56 -0400

    Accidentally removed everything

---
+++ CobaltCommands.lua
@@ -0,0 +1,47 @@
+--Created by Preston Elam (CobaltTetra) 2020
+--THIS SCRIPT IS PROTECTED UNDER AN GPLv3 LICENSE
+--ADDITIONALLY, YOU MAY EDIT THIS SCRIPT, BUT BY USING IT YOU AGREE TO NOT REMOVE THE CREDIT ON THE FIRST LINE IF IT IS RESDITRIBUTED, YOUR OWN CREDIT MAY BE ADDED ON LINE2.
+
+local M = {}
+
+
+----------------------------------------------------------EVENTS-----------------------------------------------------------
+
+--runs when the script is called.
+local function onInit()
+    
+end
+
+
+
+----------------------------------------------------------MUTATORS---------------------------------------------------------
+
+
+
+---------------------------------------------------------ACCESSORS---------------------------------------------------------
+
+
+
+---------------------------------------------------------FUNCTIONS---------------------------------------------------------
+
+local function kick(args)
+	DropPlayer(args)
+end
+
+
+
+------------------------------------------------------PUBLICINTERFACE------------------------------------------------------
+
+M.onInit = onInit
+
+----UPDATERS-----
+
+----MUTATORS-----
+
+----ACCESSORS----
+
+----FUNCTIONS----
+M.kick = kick
+
+
+return M
