da1152075b1a64ac2303ec66d883d09c4f240fd7
prestonelam2003 <prestonelam03@gmail.com>
2020-07-02 04:37:23 -0400

    Fixed dependency problem

--- CobaltEssentialsLoader.lua
+++ CobaltEssentialsLoader.lua
@@ -6,8 +6,8 @@ CE = require("Resources/server/CobaltEssentials/lua/CobaltEssentials")
 print("Loading CobaltCommands")
 CC = require("Resources/server/CobaltEssentials/lua/CobaltCommands")
 
-print("Loading CobaltServer")
-server = require("Resources/server/CobaltEssentials/lua/CobaltServer")
+--print("Loading CobaltServer")
+--server = require("Resources/server/CobaltEssentials/lua/CobaltServer")
 
 print("-------------Loading CobaltEssentials Config-------------")
 config = require("Resources/server/CobaltEssentials/lua/CobaltConfig")
